a275b6519ae4fddb9ea83f380fb92f770be5450c
BoramLove <18712768165@163.com>
2017-09-10 23:20:41 +0800

    change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,15 @@
 import React,{Component} from 'react'
 import './app.css'
 import Layout from '../Layout/Layout'
-// import Main from '../Main/Main'
-import Shopone from '../Shopone/Shopone'
+import Main from '../Main/Main'
+// import Shopone from '../Shopone/Shopone'
 // import Shoppingcar from '../Shoppingcar/Shoppingcar'
 class App extends Component{
   render(){
     return (
       <div className='app'>
         <Layout>
-        <Shopone />
+        <Main />
         </Layout>
       </div>
     )
